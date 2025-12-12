9ac5d6d8c46d431717a181e85c6a5ce38e2b9d40
Jiahui <jiahui.lin@um.es>
2025-12-12 18:04:54 +0100

    TEST3 - Se verifica la llamada al servicio de selección de dominio

--- tdd-admisiones/src/test/java/app/application/use_cases/SelectorAdmisionesUseCaseTest.java
+++ tdd-admisiones/src/test/java/app/application/use_cases/SelectorAdmisionesUseCaseTest.java
@@ -1,14 +1,18 @@
 package app.application.use_cases;
 
 import app.domain.repositories.IConvocatoriaRepository;
+import app.domain.services.ISelectorAdmisionesDomainService;
 import app.domain.services.OrdenadorInscripcionesDomainService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith; 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import app.domain.model.IConvocatoria;
 import app.domain.model.IInscripcion;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +33,11 @@ class SelectorAdmisionesUseCaseTest {
     // CUT: Clase Bajo Prueba con inyección automática
     @InjectMocks
     SelectorAdmisionesUseCase selectorUseCase;
+    
+    // Mock: Suplantamos el servicio de dominio encargado de la selección (corte)
+    @Mock
+    ISelectorAdmisionesDomainService selectorAdmisionesService;
+    
 
     @Test
     void test_GivenIdConvocatoria_WhenSeleccionar_ThenSolicitaInscripcionesAlRepositorio() {
@@ -49,6 +58,28 @@ class SelectorAdmisionesUseCaseTest {
         selectorUseCase.seleccionar(idConvocatoria);
 
         verify(ordenadorService).ordenar(inscripcionesSimuladas);
+    }
+    
+    @Test
+    void test_GivenInscripcionesOrdenadas_WhenSeleccionar_ThenLlamaAlServicioDeSeleccionConParametrosDeConvocatoria() {
+        long idConvocatoria = 1L;
+        int maxPlazas = 20;
+        double precio = 150.50;
+
+        IConvocatoria convocatoriaStub = mock(IConvocatoria.class);
+        when(convocatoriaStub.getMaxPlazas()).thenReturn(maxPlazas);
+        when(convocatoriaStub.getPrecio()).thenReturn(precio);
+
+        List<IInscripcion> inscripciones = new ArrayList<>();
+        List<IInscripcion> inscripcionesOrdenadas = new ArrayList<>();
+
+        when(convocatoriaRepository.obtenerInscripciones(idConvocatoria)).thenReturn(inscripciones);
+        when(convocatoriaRepository.obtenerConvocatoria(idConvocatoria)).thenReturn(convocatoriaStub);
+        when(ordenadorService.ordenar(inscripciones)).thenReturn(inscripcionesOrdenadas);
+
+        selectorUseCase.seleccionar(idConvocatoria);
+
+        verify(selectorAdmisionesService).seleccionar(inscripcionesOrdenadas, precio, maxPlazas);
     }
     
   
